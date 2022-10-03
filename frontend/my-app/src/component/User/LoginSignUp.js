e0856420ff4ff2124f37041c4b6b17991d948970
Shjnn <anhgcd17043@fpt.edu.vn>
2022-10-03 15:22:03 +0700

    update: Login

--- frontend/my-app/src/component/User/LoginSignUp.js
+++ frontend/my-app/src/component/User/LoginSignUp.js
@@ -1,11 +1,20 @@
-import React, { Fragment, useRef, useState, useEffect } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import "./LoginSignUp.css";
 import Loader from "../layout/Loader/Loader";
+import {Link} from "react-router-dom";
+//import LockOpenIcon from "material-ui/icon/LockOpen";
 
 const LoginSignUp = () => {
   const loginTab = useRef(null);
   const registerTab = useRef(null);
   const switcherTab = useRef(null);
+
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const loginSubmit = () => {
+    console.log("Form Submitted");
+  };
 
   const switchTabs = (e, tab) => {
     if (tab === "login") {
@@ -14,6 +23,13 @@ const LoginSignUp = () => {
 
       registerTab.current.classList.remove("shiftToNeutralForm");
       loginTab.current.classList.remove("shiftToLeft");
+    }
+    if (tab === "register") {
+        switcherTab.current.classList.add("shiftToRight");
+        switcherTab.current.classList.remove("shiftToNeutral");
+  
+        registerTab.current.classList.add("shiftToNeutralForm");
+        loginTab.current.classList.add("shiftToLeft");
     }
 
    };
@@ -28,7 +44,7 @@ const LoginSignUp = () => {
             </div>
             <button ref={switcherTab}></button>
           </div>
-          <form className="LoginForm" ref={LoginTab} onSubmit={loginSubmit}>
+          <form className="LoginForm" ref={loginTab} onSubmit={loginSubmit}>
             <div className="loginEmail">
               <input
                 type="email"
