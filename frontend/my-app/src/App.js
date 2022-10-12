92bf619eecc2960498b782ce934d8758ecb52fd8
Shjnn <anhgcd17043@fpt.edu.vn>
2022-10-12 18:46:21 +0700

    Update: UserOptions

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,18 +1,27 @@
 import "./App.css";
-import React from "react";
+import React,{useEffect} from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./component/layout/Header/Header.js";
 import Footer from "./component/layout/Footer/Footer.js";
 import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
-import Search from "./component/Product/Search.js";
 import LoginSignUp from "./component/User/LoginSignUp";
+import store from './store';
+import { loadUser } from "./actions/userAction";
+import UserOptions from "./component/layout/Header/UserOptions.js";
 
 const App = () => {
+
+  
+
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Router>
       <Header/>
+      {isAuthenticated && <UserOptions user ={user}/>}
       <Route exact path="/" component={Home} />
       <Route exact path="/product/:id" component={ProductDetails} />
       <Route exact path="/products" component={Products} />
