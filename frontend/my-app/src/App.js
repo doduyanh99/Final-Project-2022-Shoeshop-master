5c0471545059faf4bb1cd5669dcf2c32627f6a6e
Shjnn <anhgcd17043@fpt.edu.vn>
2022-10-17 06:50:05 +0700

    Update: User

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./component/layout/Header/Header.js";
 import Footer from "./component/layout/Footer/Footer.js";
@@ -7,14 +7,12 @@ import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails.js";
 import Products from "./component/Product/Products.js";
 import LoginSignUp from "./component/User/LoginSignUp";
-import store from './store';
+import store from "./store";
 import { loadUser } from "./actions/userAction";
 import UserOptions from "./component/layout/Header/UserOptions.js";
 import { useSelector } from "react-redux";
 
-
 const App = () => {
-
   const { isAuthenticated, user } = useSelector((state) => state.user);
 
   useEffect(() => {
@@ -22,8 +20,8 @@ const App = () => {
   }, []);
   return (
     <Router>
-      <Header/>
-      {isAuthenticated && <UserOptions user ={user}/>}
+      <Header />
+      {isAuthenticated && <UserOptions user={user} />}
       <Route exact path="/" component={Home} />
       <Route exact path="/product/:id" component={ProductDetails} />
       <Route exact path="/products" component={Products} />
