619a7f723ba2f67a24ca536dd2670feea669ed00
Shjnn <anhgcd17043@fpt.edu.vn>
2022-10-19 04:53:49 +0700

    Update: ProductCard

--- frontend/my-app/src/component/Product/ProductDetails.js
+++ frontend/my-app/src/component/Product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
@@ -7,6 +7,7 @@ import ReviewCard from "./ReviewCard.js";
 import { useAlert } from "react-alert";
 import Loader from "../layout/Loader/Loader";
 import ReactStars from "react-rating-stars-component";
+import addItemsToCart from "../../actions/cartAction.js"
 
 const ProductDetails = ({ match }) => {
   const dispatch = useDispatch();
@@ -16,19 +17,76 @@ const ProductDetails = ({ match }) => {
     (state) => state.productDetails
   );
 
+  const { success, error: reviewError } = useSelector(
+    (state) => state.newReview
+  );
+
+  const options = {
+    size: "large",
+    value: product.ratings,
+    readOnly: true,
+    precision: 0.5,
+  };
+
+  const [quantity, setQuantity] = useState(1);
+  const [open, setOpen] = useState(false);
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState("");
+
+  const increaseQuantity = () => {
+    if (product.Stock <= quantity) return;
+
+    const qty = quantity + 1;
+    setQuantity(qty);
+  };
+
+  const decreaseQuantity = () => {
+    if (1 >= quantity) return;
+
+    const qty = quantity - 1;
+    setQuantity(qty);
+  };
+
+  const addToCartHandler = () => {
+    dispatch(addItemsToCart(match.params.id, quantity));
+    alert.success("Item Added To Cart");
+  };
+
+  const submitReviewToggle = () => {
+    open ? setOpen(false) : setOpen(true);
+  };
+
+  const reviewSubmitHandler = () => {
+    const myForm = new FormData();
+
+    myForm.set("rating", rating);
+    myForm.set("comment", comment);
+    myForm.set("productId", match.params.id);
+
+    dispatch(newReview(myForm));
+
+    setOpen(false);
+  };
+
   useEffect(() => {
     if (error) {
       alert.error(error);
       dispatch(clearErrors());
     }
-    dispatch(getProductDetails(match.params.id));
-  }, [dispatch, match.params.id, error, alert]);
 
-  const options = {
-    value: product?.ratings,
-    readOnly: true,
-    precision: 0.5,
-  };
+    if (reviewError) {
+      alert.error(reviewError);
+      dispatch(clearErrors());
+    }
+
+    if (success) {
+      alert.success("Review Submitted Successfully");
+      dispatch({ type: NEW_REVIEW_RESET });
+    }
+    dispatch(getProductDetails(match.params.id));
+  }, [dispatch, match.params.id, error, alert, reviewError, success]);
+
+
   return (
     <>
       {loading ? (
@@ -66,9 +124,9 @@ const ProductDetails = ({ match }) => {
            
               <div className="detailsBlock-3-1">
                 <div className="detailsBlock-3-1-1">
-                  <button>-</button>
-                  <input value="1" type="number" />
-                  <button>+</button>
+                <button onClick={decreaseQuantity}>-</button>
+                    <input readOnly type="number" value={quantity} />
+                    <button onClick={increaseQuantity}>+</button>
                 </div>{" "}
                 <button>Add to cart</button>
               </div>
